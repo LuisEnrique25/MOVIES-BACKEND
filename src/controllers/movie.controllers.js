cb1f1a1b7e0b7d6b2bde9623cbf5be1dcb1fdbba
LuisEnrique25 <luisrg2225@gmail.com>
2024-02-05 09:01:33 -0600

    setGenre creada e exportada

--- src/controllers/movie.controllers.js
+++ src/controllers/movie.controllers.js
@@ -35,10 +35,23 @@ const update = catchError(async(req, res) => {
     return res.json(result[1][0]);
 });
 
+const setGenre = catchError(async(req, res) => {
+    const { id } = req.params; 
+    const movie = await Movie.findByPk(id);
+    if(!movie) return res.sendStatus(404);
+
+    await movie.setGenres(req.body);
+
+    const genres = await movie.getGenres();
+
+    return res.json(genres)
+})
+
 module.exports = {
     getAll,
     create,
     getOne,
     remove,
-    update
+    update, 
+    setGenre
 }
