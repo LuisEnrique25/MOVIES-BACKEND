0a59abafd07c42bb1b9c8f854d4c17a0326081ad
LuisEnrique25 <luisrg2225@gmail.com>
2024-02-05 08:55:29 -0600

    Relaciones moviesGenres, moviesActors, moviesDirectors creadas

--- src/models/index.js
+++ src/models/index.js
@@ -2,3 +2,15 @@ const Actor = require("./Actor")
 const Director = require("./Director")
 const Genre = require("./Genre")
 const Movie = require("./Movie")
+
+// RELACION MUCHOS A MUCHOS
+
+Movie.belongsToMany(Genre, {through: "moviesGenres"})
+Genre.belongsToMany(Movie, {through: "moviesGenres"})
+
+Movie.belongsToMany(Actor, {through: "moviesActors"})
+Actor.belongsToMany(Movie, {through: "moviesActors"})
+
+Movie.belongsToMany(Director, {through: "moviesDirectors"})
+Director.belongsToMany(Movie, {through: "moviesDirectors"})
+
